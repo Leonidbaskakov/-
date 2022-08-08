0138b4f5ff2eadf213152ec0d22ede29d6f4ee6d
leonid <bskakaw2014@yandex.ru>
2022-08-08 17:05:42 +0300

    Доделал домашнее задание

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.Write("Введите первое число ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// if (numberA>numberB)
+// {
+//   Console.WriteLine($"Число {numberA} большее, а {numberB} меньшее");
+// }
+// else
+// {
+//   Console.WriteLine($"Число {numberB} большее, а {numberA} меньшее");
+// }
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Write("Введите первое число ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число ");
+// int numberC = Convert.ToInt32(Console.ReadLine());
+// if (numberA>numberB)
+// {
+//     Console.WriteLine($"Максимальное {numberA}");
+// }
+// else 
+// if(numberB>numberC)
+// {
+//     Console.WriteLine($"Максимальное {numberB}");
+// }
+// else
+// Console.WriteLine($"Максимальное {numberC}");
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Write("Введите число ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// if ((numberA % 2) >0)
+// {
+//     Console.WriteLine($"Число {numberA} не является чётным");
+// }
+// else
+// Console.WriteLine($"Число {numberA} является чётным");
+
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("Введите число ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// for (  int numberA=1 ; numberA<=numberB ; numberA ++)
+// {
+// if (numberA % 2 == 0 )
+// {
+//     Console.Write(numberA + " ");
+// }
+// }
+
+
+// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.Write("Введите число ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// for (  int numberA=(-numberB) ; numberA<=numberB ; numberA ++)
+// Console.Write(numberA + " ");
+
+
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+// Console.Write("Введите число ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// int numberA=(numberB % 10);
+// Console.Write(numberA);
+
+
